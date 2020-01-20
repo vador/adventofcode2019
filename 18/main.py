650f26348e049e28decac7ef192e8af84810cd9a
Damien Salvador <vador@grabeuh.com>
2020-01-20 15:21:07 +0100

    Day 18 second star ! Again, lots of code for nothing. But finally a Dijkstra working !

--- 18/main.py
+++ 18/main.py
@@ -2,25 +2,44 @@ from loadValues import LoadValues
 from mazeKeys import Graph
 import cProfile, pstats
 
-
 lv = LoadValues("input")
 maze = lv.strip_lines()
 my_graph = Graph()
-my_graph.build_maze(maze)
+# my_graph.build_maze(maze)
+# print(my_graph.graph)
+# print(my_graph.key_pos)
+# print(my_graph.door_pos)
+# print(my_graph.entrance)
+
+# print(my_graph.find_reachable_keys(my_graph.entrance, []))
+# print(my_graph.find_reachable_keys((17,1), ['a']))
+# print(my_graph.find_reachable_keys((11,1), ['a','b']))
+
+# print(my_graph.find_best_path(my_graph.entrance, [], 0))
+
+# print(my_graph.find_reachable_keys((1,3), ['a','b', 'c', 'd', 'e']))
+pr = cProfile.Profile()
+
+# pr.enable()
+#
+# print(my_graph.find_best_path_from_entrance())
+#
+# pr.disable()
+# pr.print_stats()
+
+# pr2 = cProfile.Profile()
+#
+# pr2.enable()
+# print(my_graph.find_best_path_dijkstra())
+# pr2.disable()
+# pr2.print_stats()
+
+my_graph.build_maze_4_bots(maze)
 print(my_graph.graph)
 print(my_graph.key_pos)
 print(my_graph.door_pos)
 print(my_graph.entrance)
+#for key in my_graph.key_path.keys():
+print(my_graph.key_path)
 
-#print(my_graph.find_reachable_keys(my_graph.entrance, []))
-#print(my_graph.find_reachable_keys((17,1), ['a']))
-#print(my_graph.find_reachable_keys((11,1), ['a','b']))
-
-#print(my_graph.find_best_path(my_graph.entrance, [], 0))
-
-#print(my_graph.find_reachable_keys((1,3), ['a','b', 'c', 'd', 'e']))
-pr = cProfile.Profile()
-pr.enable()
-print(my_graph.find_best_path_from_entrance())
-pr.disable()
-#pr.print_stats()
+print(my_graph.find_best_path_dijkstra_4_bots())
--- 18/mazeKeys.py
+++ 18/mazeKeys.py
@@ -1,4 +1,5 @@
 import sys
+import heapq
 
 from queue import Queue
 
@@ -9,6 +10,11 @@ def is_door(val):
 
 def is_key(val):
     return 'a' <= val <= 'z'
+
+
+def is_entrance(val):
+    return '0' <= val <= '9'
+
 
 class key_list:
     _keys = None
@@ -21,38 +27,78 @@ class key_list:
 
     def are_sufficient_to_reach(self, needed_keys):
         if len(needed_keys) == 0:
-            return true
+            return True
         sufficient = True
         for key in needed_keys:
             if key not in self._keys:
                 return False
         return True
 
+
 class Graph:
     graph = None
     key_pos = None
     door_pos = None
     entrance = None
+    entrances = None
     maze = None
     key_path = None
 
     def __init__(self):
+        pass
+
+    def build_maze(self, maze):
+        self.maze = maze.copy()
+        self.string_to_maze(maze)
+        self.build_all_path_from_keys()
+
+    def convert_4_bots(self):
+        (x, y) = self.entrance[0]
+        self.key_pos.pop('@')
+        self.set_cell((x, y), '#')
+        self.entrance = []
+        neighbour_cells = [(-1, 0), (0, 1), (1, 0), (0, -1)]
+        diagon_cells = [(-1, -1), (-1, 1), (1, 1), (1, -1)]
+        for (dx, dy) in neighbour_cells:
+            self.set_cell((x+dx, y+dy), '#')
+        for i, (dx, dy) in enumerate(diagon_cells):
+            self.set_cell((x+dx, y+dy), chr(ord('1')+i))
+            self.key_pos[chr(ord('1')+i)] = (x+dx, y+dy)
+            self.entrance.append((x+dx, y+dy))
+        return self.maze
+
+    def build_maze_4_bots(self, maze):
+        self.maze = maze.copy()
+        self.string_to_maze(maze)
+        maze = self.convert_4_bots()
+        self.string_to_maze(self.maze)
+        self.build_all_path_from_keys()
+
+    def build_all_path_from_keys(self):
+        self.key_path = {}
+        for key in self.key_pos:
+            self.key_path[key] = self.build_path_from_key(key)
+
+    def string_to_maze(self, maze):
+        maxx = len(maze[0])
+        maxy = len(maze)
+        self.entrance = []
         self.graph = {}
         self.key_pos = {}
         self.door_pos = {}
 
-    def build_maze(self, maze):
-        maxx = len(maze[0])
-        maxy = len(maze)
-        self.maze = maze.copy()
         neighbour_cells = [(-1, 0), (0, 1), (1, 0), (0, -1)]
         for i in range(maxx):
             for j in range(maxy):
                 cell_value = maze[j][i]
                 if cell_value != "#":
                     if cell_value == "@":
-                        self.entrance = (i, j)
-                        self.key_pos['@'] = (i,j)
+                        self.entrance.append((i, j))
+                        self.key_pos['@'] = (i, j)
+                    elif is_entrance(cell_value):
+                        #self.entrances[int(cell_value)] = (i,j)
+                        self.key_pos[cell_value] = (i, j)
+                        self.entrance.append(((i,j)))
                     elif is_key(cell_value):
                         self.key_pos[cell_value] = (i, j)
                     elif is_door(cell_value):
@@ -63,13 +109,16 @@ class Graph:
                         if (val != "#"):
                             temp_nb.append(((i + nbx, j + nby), val))
                     self.graph[(i, j)] = (temp_nb, cell_value)
-        self.key_path = {}
-        for key in self.key_pos:
-            self.key_path[key] = self.build_path_from_key(key)
 
     def get_cell(self, pos):
         (x, y) = pos
         return self.maze[y][x]
+
+    def set_cell(self, pos, value):
+        (x, y) = pos
+        tmp_row = list(self.maze[y])
+        tmp_row[x] = value
+        self.maze[y] = "".join(tmp_row)
 
     def find_reachable_keys(self, start_key, key_list):
         reachable_keys = []
@@ -93,18 +142,18 @@ class Graph:
         while not to_visit.empty():
             (cur_cell, cur_dist, needed_keys) = to_visit.get()
             if cur_cell not in visited.keys():
-                #print((cur_cell, cur_dist))
+                # print((cur_cell, cur_dist))
                 visited[cur_cell] = cur_dist
                 (cur_nb, cell_value) = self.graph[cur_cell]
                 if is_key(cell_value):
                     path[cell_value] = (cur_dist, needed_keys)
                 for (nb, content) in cur_nb:
-                    #print("bla :", nb, content, val)
-                    if content == '.' or content =='@' or is_key(content):
-                        #print("Add ", nb, cur_dist+1)
+                    # print("bla :", nb, content, val)
+                    if content == '.' or content == '@' or is_key(content) or is_entrance(content):
+                        # print("Add ", nb, cur_dist+1)
                         to_visit.put((nb, cur_dist + 1, needed_keys))
-                    if content >= 'A' and content <= 'Z':
-                        to_visit.put((nb, cur_dist + 1, needed_keys+ [content.lower()]))
+                    if 'A' <= content <= 'Z':
+                        to_visit.put((nb, cur_dist + 1, needed_keys + [content.lower()]))
         return path
 
     def are_sufficient_to_reach(self, obtained_keys, needed_keys):
@@ -121,7 +170,7 @@ class Graph:
         res = []
         neighbour_cells = [(-1, 0), (0, 1), (1, 0), (0, -1)]
         for (dx, dy) in neighbour_cells:
-            res.append(((x+dx, y+dy), self.maze[y+dy][x+dx]))
+            res.append(((x + dx, y + dy), self.maze[y + dy][x + dx]))
         return res
 
     def is_reachable_cell(self, cell_value, keys_obtained):
@@ -136,7 +185,7 @@ class Graph:
         return False
 
     def find_best_path_from_entrance(self):
-        all_key = frozenset([key for key in self.key_pos.keys()^{'@'}])
+        all_key = frozenset([key for key in self.key_pos.keys() ^ {'@'}])
 
         visited = {}
         to_visit = Queue()
@@ -144,7 +193,7 @@ class Graph:
         to_visit.put((0, self.entrance, keys_obtained))
         while not to_visit.empty():
             (cur_dist, cur_pos, keys_obtained) = to_visit.get()
-            cur_dist +=1
+            cur_dist += 1
             for (neighbour, cell_value) in self.get_neighbours(cur_pos):
                 if self.is_reachable_cell(cell_value, keys_obtained):
                     if is_key(cell_value):
@@ -164,9 +213,93 @@ class Graph:
                 best_dist = cur_dist
         return best_dist
 
+    def find_best_path_dijkstra(self):
+        all_key = frozenset()
+        #all_key = frozenset([key for key in self.key_pos.keys()])
 
+        distances = {}
+        distances['@'] = 0
+        keys_obtained = frozenset()
 
-        return start_pos
+        pq = [((0,0), keys_obtained, '@')]
+        while len(pq)>0:
+            (cur_dist, nb_keys), obtained_keys, cur_key = heapq.heappop(pq)
+
+            #print(cur_dist, cur_key, obtained_keys)
+            cur_obtained = obtained_keys | {cur_key}
+            if (cur_key, cur_obtained) not in distances.keys():
+                distances[(cur_key, cur_obtained)] = float('infinity')
+
+            if cur_dist > distances[(cur_key, cur_obtained)]:
+                continue
+
+#    def find_reachable_keys(self, start_key, key_list):
+            (reachable, remaining) = self.find_reachable_keys(cur_key, cur_obtained)
+            for (neighbour, dist) in reachable:
+                full_dist = cur_dist + dist
+
+                tmp_obtained_keys = cur_obtained | {neighbour}
+
+                if (neighbour, tmp_obtained_keys) not in distances.keys():
+                    distances[(neighbour, tmp_obtained_keys)] = float('infinity')
+                if full_dist < distances[(neighbour, tmp_obtained_keys)]:
+                    distances[(neighbour, tmp_obtained_keys)] = full_dist
+                    heapq.heappush(pq, ((full_dist, len(cur_obtained)+1), tmp_obtained_keys, neighbour))
+        #print(distances)
+        best_dist = float('infinity')
+        all_key = frozenset([key for key in self.key_pos.keys()])
+
+        for candidate in self.key_pos.keys():
+            if (candidate, all_key) in distances.keys():
+                cur_dist = distances[(candidate, all_key)]
+                if cur_dist < best_dist:
+                    best_dist = cur_dist
+        return best_dist
+
+    def find_best_path_dijkstra_4_bots(self):
+
+        all_key = frozenset([key for key in self.key_pos.keys() if is_key(key)])
+
+        bot_pos = tuple([pos for pos in self.entrance])
+        keys_obtained = frozenset()
+
+        distances = {}
+        pq = [((0,0), keys_obtained, bot_pos)]
+        distances[(bot_pos, keys_obtained)] = 0
+
+        while len(pq)>0:
+            (cur_dist, nb_keys), obtained_keys, bot_pos = heapq.heappop(pq)
+
+            #print("Blih :" ,(cur_dist, nb_keys), len(obtained_keys), obtained_keys, bot_pos)
+
+            cur_obtained = obtained_keys
+            if cur_dist > distances[(bot_pos, cur_obtained)]:
+                #print("<> Ignored Node)
+                continue
+            for id_bot, bot in enumerate(bot_pos):
+                cur_key = self.get_cell(bot)
+                (reachable, remaining) = self.find_reachable_keys(cur_key, cur_obtained)
+                for (neighbour, dist) in reachable:
+                    full_dist = cur_dist + dist
+                    tmp_obtained_keys = cur_obtained | {neighbour}
+                    tmp_bot_pos = list(bot_pos)
+                    tmp_bot_pos[id_bot] = self.key_pos[neighbour]
+                    tmp_bot_pos = tuple(tmp_bot_pos)
+                    if (tmp_bot_pos, tmp_obtained_keys) not in distances.keys():
+                        distances[(tmp_bot_pos, tmp_obtained_keys)] = float('infinity')
+                    if full_dist < distances[(tmp_bot_pos, tmp_obtained_keys)]:
+                        distances[(tmp_bot_pos, tmp_obtained_keys)] = full_dist
+                        heapq.heappush(pq, ((full_dist, len(cur_obtained)+1), tmp_obtained_keys, tmp_bot_pos))
+
+        best_dist = float('infinity')
+        print(all_key)
+        for (candidate, obtained) in distances.keys():
+            if len(obtained)==len(all_key):
+                cur_dist = distances[(candidate, obtained)]
+                if cur_dist < best_dist:
+                    best_dist = cur_dist
+        return best_dist
+
 
 if __name__ == '__main__':
     print()
